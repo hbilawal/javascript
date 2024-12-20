49a5de04324e5c4cf9085784421b8acccaa83792
hingorobilawal <hingorobilawal786@gmail.com>
2024-12-20 10:00:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+//chapter no 4
+
+// Q 1 legal variables
+//var $dollor = "variable"
+//var str232 = "variable"
+//var engBook = "englishbook"
+
+
+//Q 2 illegal variables
+//var !exlamation = "illegalvariable"
+//var 232str = "illegalvariable"
+//var Bookeng = "illegalvariable"
+
+
+//Q 3 display this in your browser
+//var a = "A heading stating rules for naming js variables"
+//var b = "variables names can only contain numbers $ and for example $ my1stvariable" 
+//var c = "variables names are case senstive"
+//var d = "variables names should not be js keywords"
+
+chapter no 5
+
+
+//Q 1 
+//var c = "sum of 3 and 5 is 8"
+//document.write(c)
+
+
+//Q 2
+//var a = "after addition is 13"
+//document.write(a)
+
+
+//Q 4
+//var b = "initial value 5"
+//document.write(b)
+
+
+//Q 4 
+//var c = "value after increment is 6"
+//document.write(c)
+
+//Q 4
+//var d = "value after adition after 13"
+//document.write(d)
+
+//Q 5 
+//var e = "value after decrement is 12"
+//document.write(e)
+
+//Q 6
+//var f = "the remember is 0"
+//document.write(f)
+
+// Q 7
+//document.write("x" + 1 "=" 4)
+//document.write("x" + 2 "=" 8)
+//document.write("x" + 3 "=" 12)
+//document.write("x" + 4 "=" 16)
+//document.write("x" + 5 "=" 18)
+//document.write("x" + 6 "=" 22)
+//document.write("x" + 7 "=" 24)
+//document.write("x" + 8 "=" 28)
+//document.write("x" + 9 "=" 32)
+//document.write("x" + 10 "=" 36)
